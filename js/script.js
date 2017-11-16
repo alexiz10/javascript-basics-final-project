ac0de2816daa80e85cd4a9333345aff776cbe592
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-11-15 20:55:51 -0700

    Added JS code to make calculations
    Added the code necessary for the user to see statistics when the button is clicked.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 function calculate() {
 	var text = document.getElementById('all-text').value;
+	var totalWords = 0;
+	var totalSpaces = 0;
+	var totalSentences = 0;
+
+	for (let i = 0; i < text.length; i++) {
+		var c = text.charAt(i);
+		if (c === '?' || c === '.' || c === '!') {
+			totalSentences++;
+		} else if (c === ' ') {
+			totalWords++;
+			totalSpaces++;
+		}
+	}
+	totalWords++;
+
+	document.getElementById('avg-words').innerHTML = "Average words per sentence: " + totalWords / totalSentences;
+	document.getElementById('total-spaces').innerHTML = "Total spaces: " + totalSpaces;
+	document.getElementById('total-words').innerHTML = "Total words: " + totalWords;
+	document.getElementById('total-sentences').innerHTML = "Total sentences: " + totalSentences;
 }
